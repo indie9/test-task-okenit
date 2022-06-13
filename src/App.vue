604b383ec734d58d21b333f6e2bbb03d01ea1927
nikolay galanov <galanovn@gmail.com>
2022-06-13 13:59:50 +0300

    initing

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Button> press</Button>
+    <Select :list="coinsList" v-model="check"> asfasafgsa</Select>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/Button.vue";
+import { getCoins, getMinimal } from "./api/index";
+import Select from "./components/Select.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      check: {
+        featured: true,
+        hasExternalId: false,
+        image: "https://changenow.io/images/sprite/currencies/btc.svg",
+        isFiat: false,
+        isStable: false,
+        name: "Bitcoin",
+        supportsFixedRate: true,
+        ticker: "btc",
+      },
+      coinsList: [],
+    };
+  },
+  components: {
+    HelloWorld,
+    Button,
+    Select,
+  },
+  mounted() {
+    getCoins().then((data) => (this.coinsList = data));
+
+    // .then(() => getMinimal("btc", "eth"))
+    // .then((data) => (this.test = data));
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/api/index.js
@@ -0,0 +1,33 @@
+const axios = require("axios").default;
+const inst = axios.create({
+  baseURL: "https://api.changenow.io/v1",
+});
+
+export function getCoins() {
+  return inst
+    .get("/currencies")
+    .then((res) => {
+      //console.log(res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+export function getMinimal(first, second) {
+  return inst
+    .get(
+      `/min-amount/${first}_${second}?api_key=c9155859d90d239f909d2906233816b26cd8cf5ede44702d422667672b58b0cd&useRateId=true`
+    )
+    .then((res) => {
+      //console.log(res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+// export default {
+//   getCoins: getCoins(),
+//   getMinimal: getMinimal(),
+// };
